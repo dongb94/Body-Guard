7f4d975f004eee5961e3654a143de6847842b32e
Donggeon Byeon <dongb94@gmail.com>
2019-02-13 22:27:08 +0900

    Update LoggyUIMover to base on LobbyUIState property

--- Assets/Script/UI/LobbyUIMover.cs
+++ Assets/Script/UI/LobbyUIMover.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LobbyUIMover : MonoBehaviour
+public class LobbyUIMover : Singleton<LobbyUIMover>
 {
-    // Start is called before the first frame update
-    void Start()
+    private LobbyState _lobbyUIState;
+    
+    public enum LobbyState
     {
+        Main, //Start Scene
+        Option,
+        PlayerSlotSelect, // Select Game Save Slot
+        DifficultySelect, // If Select Empty Save Slot
+        StageSelect01, // Stage Map of Normal Difficulty
         
+        Credit,
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Initialize()
     {
-        
+        base.Initialize();
+
+        _lobbyUIState = LobbyState.Main;
+    }
+
+    public LobbyState LobbyUIState
+    {
+        get => _lobbyUIState;
+        set
+        {
+            if(_lobbyUIState == value) return;
+
+            switch (value)
+            {
+                case LobbyState.Main:
+                    break;
+                case LobbyState.Option:
+                    break;
+                case LobbyState.PlayerSlotSelect:
+                    break;
+                case LobbyState.DifficultySelect:
+                    break;
+                case LobbyState.StageSelect01:
+                    break;
+                case LobbyState.Credit:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+            
+            _lobbyUIState = value;
+        }
     }
 }
